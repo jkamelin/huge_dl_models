c822be266323bb633d37a44deebe88bdd8e5840e
Yuliya Kamelina <y.kamelina@yadro.com>
2023-02-02 22:22:37 +0300

    update code

--- dall-e/dalle.py
+++ dall-e/dalle.py
@@ -13,86 +13,8 @@ from functools import partial
 from dalle_mini import DalleBart, DalleBartProcessor
 from vqgan_jax.modeling_flax_vqgan import VQModel
 
-DALLE_MODEL = 'dalle-mini/dalle-mini/mega-1-fp16:latest'
+DALLE_MODEL = 'dalle-mini/dalle-mini/mini-1:v0'
 VQGAN_REPO = 'dalle-mini/vqgan_imagenet_f16_16384'
-
-
-class DalleModel:
-    def __init__(self, model, params, vqgan, vqgan_params):
-        self.model = model
-        self.params = params
-        self.vqgan = vqgan
-        self.vqgan_params = vqgan_params
-
-    @classmethod
-    def load_model(cls, dalle_model, vqgan_model, use_jax):
-        model, params = DalleBart.from_pretrained(dalle_model, dtype=np.float16, _do_init=False)
-
-        vqgan, vqgan_params = VQModel.from_pretrained(vqgan_model, _do_init=False)
-
-        if use_jax:
-            params = replicate(params)
-            vqgan_params = replicate(vqgan_params)
-
-        return cls(model, params, vqgan, vqgan_params)
-
-    @partial(jax.pmap, axis_name="batch", static_broadcasted_argnums=(3, 4, 5))
-    def p_generate(self, tokenized_prompt, key, top_k, top_p, condition_scale):
-        return self.model.generate(
-            **tokenized_prompt,
-            prng_key=key,
-            params=self.params,
-            top_k=top_k,
-            top_p=top_p,
-            condition_scale=condition_scale,
-            do_sample=True
-        )
-
-    # decode image
-    @partial(jax.pmap, axis_name="batch")
-    def p_decode(self, indices):
-        return self.vqgan.decode_code(indices, params=self.vqgan_params)
-
-    def generate_images_jax(self, tokenized_prompt, key, n_predictions=8,
-                            gen_top_k=5, gen_top_p=0.8, cond_scale=10.0):
-        images = []
-        inference_time = 0
-        iter_number = max(n_predictions // jax.device_count(), 1)
-        for _ in range(iter_number):
-            key, subkey = jax.random.split(key)
-
-            t0 = perf_counter()
-            encoded_images = self.p_generate(
-                tokenized_prompt,
-                shard_prng_key(subkey),
-                gen_top_k,
-                gen_top_p,
-                cond_scale,
-            )
-            encoded_images = encoded_images.sequences[..., 1:]
-            decoded_images = self.p_decode(encoded_images)
-            generation_time = perf_counter() - t0
-
-            inference_time += generation_time
-            for decoded_img in decoded_images:
-                images.append(decoded_img)
-
-        return images, inference_time/iter_number
-
-    def generate_images(self, tokenized_prompt, condition_scale=10.0, top_k=5, top_p=0.8):
-        images = []
-        t0 = perf_counter()
-        encoded_images = self.model.generate(**tokenized_prompt, params=self.params, top_k=top_k, top_p=top_p,
-                                             condition_scale=condition_scale, do_sample=True)
-        encoded_images = encoded_images.sequences[..., 1:]
-        decoded_images = self.vqgan.decode_code(encoded_images, params=self.vqgan_params)
-        generation_time = perf_counter() - t0
-
-        for decoded_img in decoded_images:
-            decoded_img = decoded_img.clip(0.0, 1.0) * 255
-            images.append(decoded_img)
-
-        return images, generation_time
 
 
 def cli_argument_parser():
@@ -102,7 +24,14 @@ def cli_argument_parser():
                         help="Optional. Path to dalle model")
     parser.add_argument('--vqgan_model', type=str, default=VQGAN_REPO,
                         help="Optional. Path to vqgan model")
-    parser.add_argument('--use_jax', action='store_true', help="Optional. Use jax.")
+    parser.add_argument('--n_predictions', type=int, default=8,
+                        help="Optional. Number of predictions to generate")
+    parser.add_argument('--top_k', type=int, default=5,
+                        help="Optional. Number of predictions with the highest probability")
+    parser.add_argument('--top_p', type=float, default=0.8,
+                        help="Optional. Maximum probability for predictions")
+    parser.add_argument('--cond_scale', type=float, default=10.0,
+                        help="Optional. Number of predictions to generate")
     parser.add_argument('--show', action='store_true', help="Optional. Show output.")
 
     args = parser.parse_args()
@@ -110,41 +39,89 @@ def cli_argument_parser():
     return args
 
 
-def prepare_input(prompts, dalle_model):
-    processor = DalleBartProcessor.from_pretrained(dalle_model, revision=None)
-    tokenized_prompts = processor(prompts)
+def load_model(dalle_model, vqgan_model):
+    model, params = DalleBart.from_pretrained(dalle_model, dtype=np.float16, _do_init=False)
+    vqgan, vqgan_params = VQModel.from_pretrained(vqgan_model, _do_init=False)
 
-    return tokenized_prompts
+    params = replicate(params)
+    vqgan_params = replicate(vqgan_params)
+
+    return model, params, vqgan, vqgan_params
+
+
+def generate_images(model, params, vqgan, vqgan_params, tokenized_prompt, n_predictions,
+                    cond_scale, gen_top_k, gen_top_p):
+    @partial(jax.pmap, axis_name="batch", static_broadcasted_argnums=(3, 4, 5))
+    def p_generate(tokenized_prompt, key, params, top_k, top_p, condition_scale):
+        return model.generate(
+            **tokenized_prompt,
+            prng_key=key,
+            params=params,
+            top_k=top_k,
+            top_p=top_p,
+            condition_scale=condition_scale,
+        )
+
+    @partial(jax.pmap, axis_name="batch")
+    def p_decode(indices, vqgan_params):
+        return vqgan.decode_code(indices, params=vqgan_params)
+
+    seed = random.randint(0, 2**32 - 1)
+    key = jax.random.PRNGKey(seed)
+
+    images = []
+    inference_time = 0
+    iter_number = max(n_predictions // jax.device_count(), 1)
+    for _ in range(iter_number):
+        key, subkey = jax.random.split(key)
+
+        t0 = perf_counter()
+        encoded_images = p_generate(
+            tokenized_prompt,
+            shard_prng_key(subkey),
+            params,
+            gen_top_k,
+            gen_top_p,
+            cond_scale,
+        )
+        encoded_images = encoded_images.sequences[..., 1:]
+        decoded_images = p_decode(encoded_images, vqgan_params)
+        generation_time = perf_counter() - t0
+
+        inference_time += generation_time
+        decoded_images = decoded_images.clip(0.0, 1.0).reshape((-1, 256, 256, 3))
+        for decoded_img in decoded_images:
+            images.append(decoded_img * 255)
+
+    return images, inference_time/iter_number
+
+
+def prepare_input(prompt, dalle_model):
+    processor = DalleBartProcessor.from_pretrained(dalle_model, revision=None)
+    tokenized_prompts = processor(prompt)
+    tokenized_prompt = replicate(tokenized_prompts)
+
+    return tokenized_prompt
 
 
 def main():
     args = cli_argument_parser()
-    dalle = DalleModel.load_model(args.dalle_model, args.vqgan_model, args.use_jax)
+    model, params, vqgan, vqgan_params = load_model(args.dalle_model, args.vqgan_model)
 
-    inference_time = []
-    # while True:
-    text_input =  [
+    prompts = [
         "sunset over a lake in the mountains",
         "the Eiffel tower landing on the moon",
     ]
 
-    tokenized_prompts = prepare_input(text_input, args.dalle_model)
-    if args.use_jax:
-        seed = random.randint(0, 2**32 - 1)
-        key = jax.random.PRNGKey(seed)
-        tokenized_prompt = replicate(tokenized_prompts)
-        images, time_ = dalle.generate_images_jax(tokenized_prompt, key)
-    else:
-        images, time_ = dalle.generate_images(tokenized_prompts)
-    inference_time.append(time_)
+    tokenized_prompt = prepare_input(prompts, args.dalle_model)
+    images, time = generate_images(model, params, vqgan, vqgan_params, tokenized_prompt,
+                                   args.n_predictions, args.cond_scale, args.top_k, args.top_p)
 
+    print(f'Average inference time: {time} seconds')
     if args.show:
         for img in images:
             img = np.asarray(img, dtype=np.uint8)
             cv2.imshow(img)
-
-    avg_time = np.average(inference_time)
-    print(f'Average inference time: {avg_time} seconds')
 
 
 if __name__ == '__main__':
